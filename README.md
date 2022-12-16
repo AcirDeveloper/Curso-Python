5e7332e9656343925f84cf02d8892c134ace18b4
AcirDeveloper <acirdeveloper@gmail.com>
2022-12-15 22:31:01 -0500

    Sintaxis basica tipos de operadores

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Curso-Python
+# Aprendiendo Python con Pildoras Informaticas
+
+## 1. [Introducción a Python]
+
+## 2. [Tipos de datos]
+
+## 3. [Operadores]
+
+## 4. [Estructuras de control]
---
+++ variables.py
@@ -0,0 +1,20 @@
+print(5+6)
+print(10%3)
+print(5**3)
+print(9//2)
+nombre = 5
+print(type(nombre))
+nombre = "Pildoras Informaticas"
+print(type(nombre))
+nombre = 5.2
+print(type(nombre))
+mensaje = """Esto es un mensaje
+con tres saltos
+de linea"""
+print(mensaje)
+numero1 = 5
+numero2 = 3
+if numero1 < numero2:
+    print("numero1 es mayor que numero2")
+else:
+    print("numero1 es menor que numero2")
